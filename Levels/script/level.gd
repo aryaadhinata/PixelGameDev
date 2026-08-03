cd4e289cc55795b20f378db88143143b3e195895
Arya <148077534+aryaadhinata@users.noreply.github.com>
2026-08-03 22:53:16 +0700

    create scene manager so character can move around different maps

--- Levels/script/level.gd
+++ Levels/script/level.gd
@@ -5,4 +5,10 @@ class_name Level extends Node2D
 func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerMeneger.set_as_parent(self)
+	LevelMeneger.level_load_started.connect(_free_level)
+	pass
+
+func _free_level() -> void :
+	PlayerMeneger.unparent_player(self)
+	queue_free()
 	pass

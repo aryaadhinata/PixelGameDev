619b4b288760a4f65dc044dcc22ae932ea630e1e
Arya <148077534+aryaadhinata@users.noreply.github.com>
2026-08-19 21:33:37 +0700

    add new resource item and make the plant can drop item and update what to drop for goblin and plant

--- props/plant/plant.gd
+++ props/plant/plant.gd
@@ -1,11 +1,30 @@
 class_name Plant extends Node2D
 
+const PICKUP = preload("res://Items/ItemPickUp/item_pick_up.tscn")
+
+@export var drops : Array[DropData]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$HitBox.Damaged.connect( TakeDamage)
+	$HitBox.Damaged.connect(TakeDamage)
 	pass # Replace with function body.
 
-func TakeDamage(_hurt_box : HurtBox)-> void:
+func TakeDamage(_hurt_box : HurtBox) -> void:
+	drop_items()
 	queue_free()
-	pass
+
+func drop_items() -> void:
+	if drops.size() == 0:
+		return
+
+	for d in drops.size():
+		if drops[d] == null or drops[d].item == null:
+			continue
+
+		var drop_count : int = drops[d].get_drop_count()
+		for r in drop_count:
+			var drop : ItemPickUp = PICKUP.instantiate() as ItemPickUp
+			drop.item_data = drops[d].item
+
+			get_parent().call_deferred("add_child", drop)
+			drop.global_position = global_position + Vector2(randf() * 16, randf() * 16)

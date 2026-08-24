e36c2f0a3a921d5aacdfed2d9e9e9a54fa37d4bb
Arya <148077534+aryaadhinata@users.noreply.github.com>
2026-08-24 23:57:14 +0700

    add a pushable statue and barier door

---
+++ Interactables/Dungeon/Script/barier_door.gd
@@ -0,0 +1,16 @@
+class_name BarierDoor extends Node
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	
+	pass
+
+
+func open_door() -> void:
+	animation_player.play("open_door")
+	pass
+
+func close_door() -> void:
+	animation_player.play("close_door")
+	pass
